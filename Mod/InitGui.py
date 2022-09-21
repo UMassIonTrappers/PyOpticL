968d8056a85eb1b871cc165bf0dbe0575ca1f9fc
Jacob Myers <jyers.m@gmail.com>
2022-09-21 12:59:14 -0400

    Add Workbench Files

---
+++ Mod/InitGui.py
@@ -0,0 +1,35 @@
+class MyWorkbench (Workbench):
+
+    MenuText = "My Workbench"
+    ToolTip = "A description of my workbench"
+    Icon = """paste here the contents of a 16x16 xpm icon"""
+
+    def Initialize(self):
+        """This function is executed when the workbench is first activated.
+        It is executed once in a FreeCAD session followed by the Activated function.
+        """
+        import MyModuleA, MyModuleB # import here all the needed files that create your FreeCAD commands
+        self.list = ["MyCommand1", "MyCommand2"] # A list of command names created in the line above
+        self.appendToolbar("My Commands",self.list) # creates a new toolbar with your commands
+        self.appendMenu("My New Menu",self.list) # creates a new menu
+        self.appendMenu(["An existing Menu","My submenu"],self.list) # appends a submenu to an existing menu
+
+    def Activated(self):
+        """This function is executed whenever the workbench is activated"""
+        return
+
+    def Deactivated(self):
+        """This function is executed whenever the workbench is deactivated"""
+        return
+
+    def ContextMenu(self, recipient):
+        """This function is executed whenever the user right-clicks on screen"""
+        # "recipient" will be either "view" or "tree"
+        self.appendContextMenu("My commands",self.list) # add commands to the context menu
+
+    def GetClassName(self): 
+        # This function is mandatory if this is a full Python workbench
+        # This is not a template, the returned string should be exactly "Gui::PythonWorkbench"
+        return "Gui::PythonWorkbench"
+       
+Gui.addWorkbench(MyWorkbench())
