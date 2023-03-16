d45a4200cb2569d73a01b94f23eb0a51120abd30
bobnifty <100501329+rniffenegger@users.noreply.github.com>
2023-03-16 09:09:45 -0400

    Added more beams for diffraction orders

--- Macro/modular_doublepass_beamsplitter.py
+++ Macro/modular_doublepass_beamsplitter.py
@@ -3,6 +3,8 @@ from freecadOptics import laser, layout, optomech
 
 from importlib import reload
 import math
+from math import *
+
 reload(optomech)
 reload(layout)
 reload(laser)
@@ -25,37 +27,48 @@ input_offset = 80
 
 input_y = base_dy-input_offset
 
-vert = -90
-horz = 180 
+
+left = -90
+right = left + 180
+up = 180
+down = up - 180 
+up_right = 45
+right_up = up_right-180
+left_down = up_right
+down_left = right_up
+left_up = 135
+up_left = left_up-180
+right_down = up_left
 
 layout.create_baseplate(base_dx, base_dy, base_dz, name="AOM_Splitter_Baseplate")
 
 beam = layout.add_beam_path(base_dx, input_y, -180)
-
-layout.place_element_along_beam("Input_Mirror_1", optomech.mirror_mount_k05s2, beam, 0b1, 45, 15)
-layout.place_element_along_beam("Input_Mirror_2", optomech.mirror_mount_k05s2, beam, 0b1, -135, INCH)
-
-layout.place_element_along_beam("Half_waveplate", optomech.rotation_stage_rsp05, beam, 0b1, horz, 60)
-layout.place_element_along_beam("Beam_Splitter", optomech.pbs_on_skate_mount, beam, 0b1, horz, 25)
-
-layout.place_element_along_beam("AOM_R2", optomech.isomet_1205c_on_km100pm_doublepass, beam, 0b11, -vert, 35)
-
-layout.place_element_along_beam("Quarter_waveplate", optomech.rotation_stage_rsp05, beam, 0b11, -90, 70)
-
-layout.place_element_along_beam("f_100_Collimation_Lens", optomech.lens_holder_l05g, beam, 0b11, vert, 30)
-
-layout.place_element_along_beam("Iris", optomech.pinhole_ida12, beam, 0b11, vert, 15)
-
-layout.place_element_along_beam("Retro_Mirror", optomech.mirror_mount_k05s2, beam, 0b11, -vert, 15)
-
-layout.place_element_along_beam("Output_Mirror_1", optomech.mirror_mount_k05s2, beam, 0b110, -45, 30)
-layout.place_element_along_beam("Output_Mirror_2", optomech.mirror_mount_k05s2, beam, 0b110, -45-90, 60)
+aom_beam_plus1 = layout.add_beam_path(INCH, input_y-10, left-0.026*180/pi)  #https://isomet.com/PDF%20acousto-optics_modulators/data%20sheets-moduvblue/M1250-T250L-0.45.pdf
+aom_beam_minus1 = layout.add_beam_path(INCH, input_y-10, left+0.026*180/pi)  #https://isomet.com/PDF%20acousto-optics_modulators/data%20sheets-moduvblue/M1250-T250L-0.45.pdf
+aom_beam_plus2 = layout.add_beam_path(INCH, input_y-10, right+0.026*180/pi)  #https://isomet.com/PDF%20acousto-optics_modulators/data%20sheets-moduvblue/M1250-T250L-0.45.pdf
+aom_beam_minus2 = layout.add_beam_path(INCH, input_y-10, right-0.026*180/pi)  #https://isomet.com/PDF%20acousto-optics_modulators/data%20sheets-moduvblue/M1250-T250L-0.45.pdf
 
 
-layout.place_element_along_beam("Iris", optomech.pinhole_ida12, beam, 0b110, vert, 150)
-layout.place_element_along_beam("Half_waveplate_Out", optomech.rotation_stage_rsp05, beam, 0b110, vert, 30)
+layout.place_element_along_beam("Input_Mirror_1", optomech.mirror_mount_k05s2, beam, 0b1, up_right, 15)
+layout.place_element_along_beam("Input_Mirror_2", optomech.mirror_mount_k05s2, beam, 0b1, right_up,  INCH)
 
-layout.place_element_along_beam("Output_Fiberport", optomech.fiberport_holder, beam, 0b110, 90, y=0)
+layout.place_element_along_beam("Half_waveplate", optomech.rotation_stage_rsp05, beam, 0b1, up, 55)
+layout.place_element_along_beam("Beam_Splitter", optomech.pbs_on_skate_mount, beam, 0b1, up, 25)
+
+layout.place_element_along_beam("AOM_R2", optomech.isomet_1205c_on_km100pm_doublepass, beam, 0b11, right, 35)
+layout.place_element_along_beam("Quarter_waveplate", optomech.rotation_stage_rsp05, beam, 0b11, left, 60)
+layout.place_element_along_beam("f_100_Collimation_Lens", optomech.lens_holder_l05g, beam, 0b11, left, 40)
+layout.place_element_along_beam("Iris", optomech.pinhole_ida12, beam, 0b11, left, 15)
+layout.place_element_along_beam("Retro_Mirror", optomech.mirror_mount_k05s2, beam, 0b11, right, 15)
+
+layout.place_element_along_beam("Output_Mirror_1", optomech.mirror_mount_k05s2, beam, 0b110, right_down, 30)
+# layout.place_element_along_beam("Iris", optomech.pinhole_ida12, beam, 0b110, down, 25)
+layout.place_element_along_beam("Output_Mirror_2", optomech.mirror_mount_k05s2, beam, 0b110, down_left, 55)
+
+
+layout.place_element_along_beam("Half_waveplate_Out", optomech.rotation_stage_rsp05, beam, 0b110, left, 150)
+
+layout.place_element_along_beam("Output_Fiberport", optomech.fiberport_holder, beam, 0b110, right, y=0)
 
 
 layout.place_element("Mount_Hole", optomech.baseplate_mount, (5-0.5)*INCH-gap/2, (1-0.5)*INCH-gap/2, 0)
