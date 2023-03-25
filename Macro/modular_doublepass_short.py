b2e38cf56e93566c7c2f340de0ecf81ec321669b
bobnifty <100501329+rniffenegger@users.noreply.github.com>
2023-03-25 14:01:01 -0400

    part numbers and km05 mounts

--- Macro/modular_doublepass_short.py
+++ Macro/modular_doublepass_short.py
@@ -19,7 +19,7 @@ base_split = INCH/4
 
 gap = 0.5*INCH
 base_dx = 5.0*INCH-gap
-base_dy = 8.0*INCH-gap
+base_dy = 8.0*INCH+10-gap
 base_dz = INCH
 
 input_y = base_dy-3.0*INCH+gap/2
@@ -45,26 +45,27 @@ layout.create_baseplate(base_dx, base_dy, base_dz, name=name)
 beam = layout.add_beam_path(base_dx, input_y, -180)
 AOM_location_x = 0.75*INCH
 # aom_beam_minus1 = layout.add_beam_path(AOM_location_x, input_y-10, left+0.026*180/pi)  #https://isomet.com/PDF%20acousto-optics_modulators/data%20sheets-moduvblue/M1250-T250L-0.45.pdf
+mirror_mounts = optomech.mirror_mount_km05
 
 
-layout.place_element_along_beam("Input_Mirror_1", optomech.mirror_mount_k05s2, beam, 0b1, up_right, 15)
-layout.place_element_along_beam("Input_Mirror_2", optomech.mirror_mount_k05s2, beam, 0b1, right_up,  INCH)
-layout.place_element_along_beam("Half_waveplate", optomech.rotation_stage_rsp05, beam, 0b1, up, 55)
+layout.place_element_along_beam("Input_Mirror_1", mirror_mounts, beam, 0b1, up_right, 15)
+layout.place_element_along_beam("Input_Mirror_2", mirror_mounts, beam, 0b1, right_up,  INCH)
+layout.place_element_along_beam("Half_waveplate", optomech.rotation_stage_rsp05, beam, 0b1, up, 55, wave_plate_part_num = '') #421nm custom waveplates from CASIX
 layout.place_element_along_beam("Beam_Splitter", optomech.pbs_on_skate_mount, beam, 0b1, up, 25)
 
 layout.place_element_along_beam("AOM", optomech.isomet_1205c_on_km100pm, beam, 0b11, right, 30, diff_angle=0)
-layout.place_element_along_beam("Quarter_waveplate", optomech.rotation_stage_rsp05, beam, 0b11, left, 45)
-layout.place_element_along_beam("f_75_Collimation_Lens", optomech.lens_holder_l05g, beam, 0b11, left, 30)
+layout.place_element_along_beam("Quarter_waveplate", optomech.rotation_stage_rsp05, beam, 0b11, left, 45, wave_plate_part_num = '') #421nm custom waveplates from CASIX
+layout.place_element_along_beam("Lens_f_75mm_A_coat", optomech.lens_holder_l05g, beam, 0b11, left, 30, lens_part_num='LA1257-A')
 layout.place_element_along_beam("Iris", optomech.pinhole_ida12, beam, 0b11, right, 10)
-layout.place_element_along_beam("Retro_Mirror", optomech.mirror_mount_k05s2, beam, 0b11, right, 10)
+layout.place_element_along_beam("Retro_Mirror", mirror_mounts, beam, 0b11, right, 10)
 
-layout.place_element_along_beam("Output_Mirror_1", optomech.mirror_mount_k05s2, beam, 0b110, right_down, 20)
+layout.place_element_along_beam("Output_Mirror_1", mirror_mounts, beam, 0b110, right_down, 20)
 # layout.place_element_along_beam("Iris", optomech.pinhole_ida12, beam, 0b110, down, 25)
-layout.place_element_along_beam("Output_Mirror_2", optomech.mirror_mount_k05s2, beam, 0b110, down_left, 55)
-layout.place_element_along_beam("Half_waveplate_Out", optomech.rotation_stage_rsp05, beam, 0b110, left, 100)
+layout.place_element_along_beam("Output_Mirror_2", mirror_mounts, beam, 0b110, down_left, 55)
+layout.place_element_along_beam("Half_waveplate_Out", optomech.rotation_stage_rsp05, beam, 0b110, left, 100, wave_plate_part_num = '') #421nm custom waveplates from CASIX
 layout.place_element_along_beam("Output_Fiberport", optomech.fiberport_holder, beam, 0b110, right, y=0)
 
-offset = 0
+offset = -10/INCH
 layout.place_element("Mount_Hole", optomech.baseplate_mount, (2-offset)*INCH-gap/2, (1-offset)*INCH-gap/2, 0)
 layout.place_element("Mount_Hole", optomech.baseplate_mount, (4-offset)*INCH-gap/2, (1-offset)*INCH-gap/2, 0)
 layout.place_element("Mount_Hole", optomech.baseplate_mount, (3-offset)*INCH-gap/2, (4-offset)*INCH-gap/2, 0)
