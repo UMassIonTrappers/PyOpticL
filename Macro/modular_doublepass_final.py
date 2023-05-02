a409ff4f88b0fa4492115d02f7ca0281a7df82ee
Jyers <jyers.m@gmail.com>
2023-05-02 11:23:06 -0400

    Doublepass Final Cleanup

--- Macro/modular_doublepass_final.py
+++ Macro/modular_doublepass_final.py
@@ -1,97 +1,51 @@
-import FreeCAD as App
-import FreeCADGui as Gui
 from freecadOptics import laser, layout, optomech
 
-from importlib import reload
-import math
 from math import *
-import os
 import datetime
 from datetime import datetime
 
-reload(optomech)
-reload(layout)
-reload(laser)
-
 INCH = 25.4
-
 aom_dy = 70
 base_split = INCH/4
-
 gap = 0.5*INCH
-base_dx = 9.0*INCH-gap #MAX size of resin printer
+base_dx = 9.0*INCH-gap
 base_dy = 5.15*INCH-gap
 base_dz = INCH
 split_dx = 79
-
 grid_offset = 15
 input_x = 5*INCH+grid_offset
-
-''' 'Cardinal' beam directions'''
-down = -90 
-up = down+ 180
-left = down - 90
-right = left + 180
-
-# Turns
-up_right = -45
-right_up = up_right-180
-left_down = up_right
-down_left = right_up
-left_up = up_right +90
-up_left = left_up-180
-right_down = up_left
-
-
-"""
-Start BASEPLATE
-"""
 
 name = "Doublepass_Resin"
 date_time = datetime.now().strftime("%m/%d/%Y")
 label = name + " " +  date_time
-print("label name date and time:",label)
+
 #layout.create_baseplate(base_dx, base_dy, base_dz, name=name, label=label) # Full plate
-layout.create_baseplate(split_dx-gap/4, base_dy, base_dz, name=name, label=label)
-layout.create_baseplate(base_dx-split_dx-gap/4, base_dy, base_dz, x=split_dx+gap/4, name=name, label=label)
+layout.create_baseplate(split_dx-gap/4, base_dy, base_dz, name=name+'1', label=label)
+layout.create_baseplate(base_dx-split_dx-gap/4, base_dy, base_dz, x=split_dx+gap/4, name=name+'2', label=label)
 
 
-'''
-Add Beam(s)
-'''
-beam = layout.add_beam_path(input_x, 0, up)
+beam = layout.add_beam_path(input_x, 0, layout.cardinal['up'])
 
-"""
-Add Optical Elements
-"""
 mirror_mounts = optomech.mirror_mount_km05
-layout.place_element("Input_Fiberport", optomech.fiberport_holder, input_x, 0, up)
-layout.place_element_along_beam("Input_Mirror_1", mirror_mounts, beam, 0b1, up_right, 19)
-layout.place_element_along_beam("Input_Mirror_2", mirror_mounts, beam, 0b1, right_up,  INCH)
-layout.place_element_along_beam("Half_waveplate", optomech.rotation_stage_rsp05, beam, 0b1, up, 55, wave_plate_part_num = '') #421nm custom waveplates from CASIX
-layout.place_element_along_beam("Beam_Splitter", optomech.pbs_on_skate_mount, beam, 0b1, up, 25)
+layout.place_element("Input_Fiberport", optomech.fiberport_holder, input_x, 0, layout.cardinal['up'])
+layout.place_element_along_beam("Input_Mirror_1", mirror_mounts, beam, 0b1, layout.turn['up-right'], 19)
+layout.place_element_along_beam("Input_Mirror_2", mirror_mounts, beam, 0b1, layout.turn['right-up'],  INCH)
+layout.place_element_along_beam("Half_waveplate", optomech.rotation_stage_rsp05, beam, 0b1, layout.cardinal['up'], 55, wave_plate_part_num = '') #421nm custom waveplates from CASIX
+layout.place_element_along_beam("Beam_Splitter", optomech.pbs_on_skate_mount, beam, 0b1, layout.cardinal['up'], 25)
 
-layout.place_element_along_beam("AOM", optomech.isomet_1205c_on_km100pm, beam, 0b11, right, 30,  diff_dir=(-1,1), exp=True)
-layout.place_element_along_beam("Quarter_waveplate", optomech.rotation_stage_rsp05, beam, 0b110, left, 70, wave_plate_part_num = '') #421nm custom waveplates from CASIX
-lens = layout.place_element_along_beam("Lens_f_100mm_AB_coat", optomech.lens_holder_l05g, beam, 0b110, left, 30, foc_len=100, lens_part_num='LA1213-AB')
-layout.place_element_along_beam("Iris", optomech.pinhole_ida12, beam, 0b111, right, 7, pre_refs=2, drill_offset=-2)
-layout.place_element_relative("Retro_Mirror", mirror_mounts, lens, right, -7)
+layout.place_element_along_beam("AOM", optomech.isomet_1205c_on_km100pm, beam, 0b11, layout.cardinal['right'], 30,  diff_dir=(-1,1), exp=True)
+layout.place_element_along_beam("Quarter_waveplate", optomech.rotation_stage_rsp05, beam, 0b110, layout.cardinal['left'], 70, wave_plate_part_num = '') #421nm custom waveplates from CASIX
+lens = layout.place_element_along_beam("Lens_f_100mm_AB_coat", optomech.lens_holder_l05g, beam, 0b110, layout.cardinal['left'], 30, foc_len=100, lens_part_num='LA1213-AB')
+layout.place_element_along_beam("Iris", optomech.pinhole_ida12, beam, 0b111, layout.cardinal['right'], 7, pre_refs=2, drill_offset=-2)
+layout.place_element_relative("Retro_Mirror", mirror_mounts, lens, layout.cardinal['right'], -7)
 
-layout.place_element_along_beam("Output_Mirror_1", mirror_mounts, beam, 0b11110, right_down, 25)
-layout.place_element_along_beam("Output_Mirror_2", mirror_mounts, beam, 0b11110, down_left, 56)
-layout.place_element_along_beam("Half_waveplate_Out", optomech.rotation_stage_rsp05, beam, 0b11110, left, 100, wave_plate_part_num = '') #421nm custom waveplates from CASIX
-layout.place_element_along_beam("Output_Fiberport", optomech.fiberport_holder, beam, 0b11110, right, x=0)
+layout.place_element_along_beam("Output_Mirror_1", mirror_mounts, beam, 0b11110, layout.turn['right-down'], 25)
+layout.place_element_along_beam("Output_Mirror_2", mirror_mounts, beam, 0b11110, layout.turn['down-left'], 56)
+layout.place_element_along_beam("Half_waveplate_Out", optomech.rotation_stage_rsp05, beam, 0b11110, layout.cardinal['left'], 100, wave_plate_part_num = '') #421nm custom waveplates from CASIX
+layout.place_element_along_beam("Output_Fiberport", optomech.fiberport_holder, beam, 0b11110, layout.cardinal['right'], x=0)
 
 
-"""
-Add holes to baseplate to mount to optical table 
- >>> Make sure to align laser beam above bolt holes so plates line up together <<<
-"""
 for i in [[0,0],[0,2],[4,1],[7,2]]:
-    layout.place_element("Mount_Hole", optomech.baseplate_mount, (i[0])*INCH+grid_offset, (i[1])*INCH+grid_offset, 0)
+    layout.place_element("Mount_Hole%s"%(str(i)), optomech.baseplate_mount, (i[0])*INCH+grid_offset, (i[1])*INCH+grid_offset, 0)
 
-"""
-Set view and compute baseplate
-"""
-#Gui.runCommand('TopViewFit',0)
 layout.redraw()
