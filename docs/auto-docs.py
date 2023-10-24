6e05f00a1c7ab2fd0d1af154cf9a837e2860126c
Jyers <jyers.m@gmail.com>
2023-10-24 17:15:18 -0400

    Update auto-docs

--- docs/auto-docs.py
+++ docs/auto-docs.py
@@ -6,15 +6,13 @@ matches = re.findall("class ([^ ]*?):\n +\'\'\'(.*?)\'\'\'", optomech_file, re.D
 
 optomech_docs = open("docs/README.md", 'w')
 
-print(matches)
-
 optomech_docs.write('''
 Testing
 ''')
 
 for i in matches:
     class_name = i[0]
-    docstring = i[1].replace("\n", "  \n")
+    docstring = i[1].replace("\n", "  \n").replace("\n    ", "\n")
     optomech_docs.write("### %s\n"%(class_name))
     optomech_docs.write("%s\n"%(docstring))
 
