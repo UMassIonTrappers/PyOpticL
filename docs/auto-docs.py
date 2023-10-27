513f5f88c13c43b263f46e65510bf85b61664ec4
Jyers <jyers.m@gmail.com>
2023-10-27 13:22:46 -0400

    Add layout to auto-docs

--- docs/auto-docs.py
+++ docs/auto-docs.py
@@ -1,17 +1,38 @@
 import re
 
+layout_file = "".join(open("Mod/Optics/freecadOptics/layout.py", 'r').readlines())
 optomech_file = "".join(open("Mod/Optics/freecadOptics/optomech.py", 'r').readlines())
+docs = open("docs/README.md", 'w')
 
-matches = re.findall("class ([^ ]*?):\n +\'\'\'(.*?)\'\'\'", optomech_file, re.DOTALL)
+docs.write('''# Auto-Generated Documentation  \n''')
 
-optomech_docs = open("docs/README.md", 'w')
+docs.write('''## Layout  \n''')
 
-optomech_docs.write('''## Auto-Generated Optomech Library Documentation  \n''')
+classes = re.findall("class ([^ ]*?):\n +\'\'\'(.*?)\'\'\'", layout_file, re.DOTALL)
+functions = re.findall("def (?!__)([^ ]*?)\(self.*?\):\n +\'\'\'(.*?)\'\'\'", layout_file, re.DOTALL)
 
-for i in matches:
-    class_name = i[0]
+print(functions[0])
+
+for i in classes:
+    name = i[0]
     docstring = i[1].replace("\n", "  \n")
-    optomech_docs.write("### %s\n"%(class_name))
-    optomech_docs.write("%s\n"%(docstring))
+    docs.write("### %s\n"%(name))
+    docs.write("%s\n"%(docstring))
 
-optomech_docs.close()
+for i in functions:
+    name = i[0]
+    docstring = i[1].replace("\n", "  \n")
+    docs.write("### baseplate.%s\n"%(name))
+    docs.write("%s\n"%(docstring))
+
+docs.write('''## Optomech  \n''')
+
+classes = re.findall("class ([^ ]*?):\n +\'\'\'(.*?)\'\'\'", optomech_file, re.DOTALL)
+
+for i in classes:
+    name = i[0]
+    docstring = i[1].replace("\n", "  \n")
+    docs.write("### %s\n"%(name))
+    docs.write("%s\n"%(docstring))
+
+docs.close()
