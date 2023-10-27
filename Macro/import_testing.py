4cd5d2454047f5fee8e9163696129d1c83f0e2e4
Jyers <jyers.m@gmail.com>
2023-10-27 13:34:07 -0400

    Isolating Repo to Workbench Folder

--- Macro/import_testing.py
+++
@@ -1,9 +0,0 @@
-import FreeCAD as App
-from freecadOptics import laser, layout, optomech
-
-#layout.place_element("Input_Mirror_1", optomech.mount_mk05pm, 0, 0, 0)
-layout.place_element("Input_Mirror_2", optomech.periscope, 0, 0, 0, lower_dz=3*layout.INCH/2, upper_dz=100, table_mount=True)
-
-#testing
-
-layout.redraw()
