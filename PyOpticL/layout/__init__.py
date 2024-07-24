77d9510c7f96e915354032ba9b35a127c3af3cf1
Justin Oh <justinoh4116@gmail.com>
2024-07-24 13:22:15 -0400

    dictionary of normals in 2d

--- PyOpticL/layout/__init__.py
+++ PyOpticL/layout/__init__.py
@@ -1,3 +1,15 @@
 from .origin import Origin
 from .baseplate import Baseplate
 from .optomech import *
+import FreeCAD as App
+
+NORMALS = {
+        'up-right': App.Vector(1, -1, 0).normalize(),
+        'up-left': App.Vector(-1, -1, 0).normalize(),
+        'right-down': App.Vector(-1, -1, 0).normalize(),
+        'right-up': App.Vector(-1, 1, 0).normalize(),
+        'down-left': App.Vector(-1, 1, 0).normalize(),
+        'down-right': App.Vector(1, 1, 0).normalize(),
+        'left-up': App.Vector(1, 1, 0).normalize(),
+        'left-down': App.Vector(1, -1, 0).normalize(),
+        }
