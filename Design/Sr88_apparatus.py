6e81b68db7dd49c5ef4f70bd62c4e6ea3389c3a5
UMass Ion Trappers <103533593+UMassIonTrappers@users.noreply.github.com>
2025-01-26 18:30:49 -0500

    update

--- Design/Sr88_apparatus.py
+++ Design/Sr88_apparatus.py
@@ -11,11 +11,11 @@ from Raman_subsystem import Raman_subsystem
 from Photoionization_subsystem import PI_subsystem_ECDL, PI_subsystem_commercial
 
 
-layout.table_grid(dx=50, dy=92)
+layout.table_grid(dx=52, dy=92)
 # layout.place_element_on_table("chamber", optomech.Room_temp_chamber_Mechanical, x=28, y = 60, angle=0) 
 # # there is a room temperature chamber in the center. it will largely increase loading time... 
-laser_cooling_subsystem(x=-3, y=0, thumbscrews=True)
-Raman_subsystem(x=-1 , y=25, thumbscrews=True)
-PI_subsystem_commercial(x=27 , y=8, angle = 0, thumbscrews=True) #405 for sr88+ 
-PI_subsystem_ECDL(x=36 , y=8, thumbscrews=True) # 461 for sr88+
-subsystem_spam(x=30 , y=50, thumbscrews=True)
+laser_cooling_subsystem(x=-1, y=0, thumbscrews=True)
+Raman_subsystem(x=1 , y=25, thumbscrews=True)
+PI_subsystem_commercial(x=29 , y=8, angle = 0, thumbscrews=True) #405 for sr88+ 
+PI_subsystem_ECDL(x=38 , y=8, thumbscrews=True) # 461 for sr88+
+subsystem_spam(x=32 , y=50, thumbscrews=True)
