0c63c7b264a925d406315af101c87157a69e3ecb
UMass Ion Trappers <103533593+UMassIonTrappers@users.noreply.github.com>
2025-01-29 17:26:34 -0500

    update

--- Design/Sr88_apparatus.py
+++ Design/Sr88_apparatus.py
@@ -15,7 +15,7 @@ layout.table_grid(dx=52, dy=92)
 # layout.place_element_on_table("chamber", optomech.Room_temp_chamber_Mechanical, x=28, y = 60, angle=0) 
 # # there is a room temperature chamber in the center. it will largely increase loading time... 
 laser_cooling_subsystem(x=-1, y=0, thumbscrews=True)
-Raman_subsystem(x=1 , y=25, thumbscrews=True)
-PI_subsystem_commercial(x=29 , y=8, angle = 0, thumbscrews=True) #405 for sr88+ 
-PI_subsystem_ECDL(x=38 , y=8, thumbscrews=True) # 461 for sr88+
-subsystem_spam(x=32 , y=50, thumbscrews=True)
+Raman_subsystem(x=1 , y=26.5, thumbscrews=True)
+# PI_subsystem_commercial(x=29 , y=8, angle = 0, thumbscrews=True) #405 for sr88+ 
+# PI_subsystem_ECDL(x=38 , y=8, thumbscrews=True) # 461 for sr88+
+# subsystem_spam(x=32 , y=50, thumbscrews=True)
--- Design/Subsystem/Raman_subsystem.py
+++ Design/Subsystem/Raman_subsystem.py
@@ -17,15 +17,15 @@ print("current wavelength is " + str(wavelength * 1e6) + " nm")
 print("current littrow angle is " + str(littrow_angle))
 def Raman_subsystem(x=0, y=0, angle=0, thumbscrews=True, littrow_angle = littrow_angle):
 # # #422 Raman 
-    # ECDL(x = 6.7 + x, y = 26 + y, angle= 270 + angle, littrow_angle = littrow_angle)    
+    ECDL(x = 6.7 + x, y = 26 + y, angle= 270 + angle, littrow_angle = littrow_angle)    
     
-    # ECDL_isolator_baseplate(x=4 + x, y=20.5 + y, angle=270+angle)
+    ECDL_isolator_baseplate(x=4 + x, y=20.5 + y, angle=270+angle)
     singlepass(x=11 + x, y=14 + y, angle = 180+angle, thumbscrews=thumbscrews)
     singlepass(x=10+x, y=9+y, angle = 180+angle, thumbscrews=thumbscrews)
-    Beam_Combiner_General(x=12+x, y=14+y, angle = 90+angle)
+    Beam_Combiner_General(x=11+x, y=13.5+y, angle = -90+angle)
     singlepass(x=9+x, y=4+y, angle = 180+angle, thumbscrews=thumbscrews)
-    Beam_Combiner_General(x=11+x, y=4+y, angle = 90+angle)
-    # Beam_pickoff(x=1.5+x, y= -1.5+y, angle=-90+angle, thumbscrews= thumbscrews)
+    Beam_Combiner_General(x=11+x, y=6.5+y, angle = -90+angle)
+    Beam_pickoff(x=1.5+x, y= -1.5+y, angle=-90+angle, thumbscrews= thumbscrews)
 
 if __name__ == "__main__":
     Raman_subsystem()
