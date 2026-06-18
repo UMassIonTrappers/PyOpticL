3ab80803ddf90e174a5af4aa063419062211daed
rniffenegger <rniffenegger@umass.edu>
2026-06-18 14:55:48 -0400

    condensed code format

--- Designs/subsystems/laser_cooling_subsystem.py
+++ Designs/subsystems/laser_cooling_subsystem.py
@@ -1,7 +1,6 @@
 from PyOpticL.layout import Layout
 from PyOpticL.utils import Dimension as dim
 from PyOpticL.utils import fix_relative_imports
-
 fix_relative_imports("../modules")
 
 from doublepass import doublepass
@@ -10,48 +9,16 @@ from ecdl.ecdl_isolator_plate import ecdl_isolator_plate
 from scalable_rb_sas.scalable_rb_sas import rb_sas
 from singlepass import singlepass
 
-
 def laser_cooling_subsystem():
 
     subsystem = Layout("Laser Cooling Subsystem")
-
     x = 0
-    subsystem.add(
-        ecdl(),
-        position=(0, 0, 0),
-        rotation=180,
-    )
-
-    x = x + 5
-    subsystem.add(
-        ecdl_isolator_plate(),
-        position=(dim(-x, "grid"), dim(3, "grid"), 0),
-        rotation=180,
-    )
-
-    x = x + 8
-    subsystem.add(
-        rb_sas(),
-        position=(dim(-x, "grid"), dim(-15, "grid"), 0),
-        rotation=90,
-    )
-
-    x = x + 7
-    subsystem.add(
-        singlepass(),
-        position=(dim(-x, "grid"), dim(-4, "grid"), 0),
-        rotation=90,
-    )
-
-    x = x + 6
-    subsystem.add(
-        doublepass(),
-        position=(dim(-x, "grid"), dim(-4, "grid"), 0),
-        rotation=90,
-    )
-
+    subsystem.add(ecdl(),                   position=(0, 0, 0),rotation=180,); x += 5
+    subsystem.add(ecdl_isolator_plate(),    position=(dim(-x, "grid"), dim(3, "grid"), 0),  rotation=180,)  ; x += 8
+    subsystem.add(rb_sas(),                 position=(dim(-x, "grid"), dim(-15, "grid"), 0),rotation=90,)   ; x += 7
+    subsystem.add(singlepass(),             position=(dim(-x, "grid"), dim(-4, "grid"), 0), rotation=90,)   ; x += 6
+    subsystem.add(doublepass(),             position=(dim(-x, "grid"), dim(-4, "grid"), 0), rotation=90,)
     return subsystem
-
 
 if __name__ == "__main__":
     subsystem = laser_cooling_subsystem()
